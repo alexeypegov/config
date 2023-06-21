b71754f6fc167525de3eb1c08e9c8471ba8269ba
Alexey Pegov <iam@alexeypegov.com>
2023-06-21 09:45:18 +0700

    add .girconfig to links.sh

--- links.sh
+++ links.sh
@@ -9,3 +9,4 @@ ln -s ~/config/.config/helix/config.toml ~/.config/helix
 ln -s ~/config/.config/helix/languages.toml ~/.config/helix
 ln -s ~/config/.config/kitty/theme.conf ~/.config/kitty
 ln -s ~/config/.config/kitty/kitty.conf ~/.config/kitty
+ln -s ~/config/.gitconfig ~/.gitconfig
