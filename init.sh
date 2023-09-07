e3dcb8d6270ee09da4c187e369cff18507e6ca71
Alexey Pegov <iam@alexeypegov.com>
2023-09-07 10:29:01 +0200

    helix +autocomplete & other

--- init.sh
+++ init.sh
@@ -5,8 +5,11 @@ ln -s ~/config/.tmux.conf ~
 ln -s ~/config/.vimrc ~
 ln -s ~/config/.zshrc ~
 ln -s ~/config/.ghci ~
+mkdir ~/.config
+mkdir ~/.config/helix
 ln -s ~/config/.config/helix/config.toml ~/.config/helix
 ln -s ~/config/.config/helix/languages.toml ~/.config/helix
+mkdir ~/.config/kitty
 ln -s ~/config/.config/kitty/theme.conf ~/.config/kitty
 ln -s ~/config/.config/kitty/kitty.conf ~/.config/kitty
 ln -s ~/config/.gitconfig ~
