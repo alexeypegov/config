62a1f0e18da7132269d7230f15a707601530f840
Alexey Pegov <alexey@pegov.dev>
2025-01-10 13:06:48 +0100

    kitty & fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,10 @@ if status is-interactive
 
     abbr --add fconf "hx ~/.config/fish/config.fish"
     abbr --add gst "git status"
-    abbr --add ga "git add ."
+    abbr --add ga "git add"
+    abbr --add gp "git push"
+    abbr --add gd "git diff"
+    abbr --add gc "git commit"
 
     set -gx EDITOR hx
 end
