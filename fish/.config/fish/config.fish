14f9f6d1070e2934fb985b45808ec3c24f65c575
Alexey Pegov <alexey@pegov.dev>
2025-05-27 14:15:31 +0200

    fish git fzf functions + gb command to quickly switch branches

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,16 @@ if status is-interactive
 
     abbr --add fconf "hx ~/.config/fish/config.fish"
 
+    # fisher install tsub/fish-fzf-git-recent-branch
+    abbr --add gb fzf_git_recent_branch
+
     set -gx EDITOR hx
+
+    if test -f ~/.config/fish/git.fish
+        echo "enable git fzf"
+        source ~/.config/fish/git.fish
+        git_fzf_key_bindings
+    end
 end
 
 # Created by `pipx` on 2025-03-15 13:26:48
